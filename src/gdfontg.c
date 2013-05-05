faf27d55008af2abeac9ef2551b31124b37bff7d
Ondřej Surý <ondrej@sury.org>
2013-05-05 10:21:49 +0200

    Imported Upstream version 2.1.0~alpha~7+292b0a

--- src/gdfontg.c
+++ src/gdfontg.c
@@ -13,7 +13,7 @@
 
 #include "gdfontg.h"
 
-char gdFontGiantData[] = {
+static const char gdFontGiantData[] = {
 	/* Char 0 */
 	0, 0, 0, 0, 0, 0, 0, 0, 0,
 	0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -4369,7 +4369,7 @@ char gdFontGiantData[] = {
 
 };
 
-gdFont gdFontGiantRep = {
+static const gdFont gdFontGiantRep = {
 	256,
 	0,
 	9,
@@ -4377,7 +4377,7 @@ gdFont gdFontGiantRep = {
 	gdFontGiantData
 };
 
-BGD_EXPORT_DATA_PROT gdFontPtr gdFontGiant = &gdFontGiantRep;
+BGD_EXPORT_DATA_PROT static const gdFontPtr gdFontGiant = &gdFontGiantRep;
 
 BGD_DECLARE(gdFontPtr) gdFontGetGiant (void)
 {
